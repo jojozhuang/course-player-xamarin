b19a1cb6fd7b971770bd76d52f7fc5b32def626c
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-29 22:09:50 -0700

    init, move from Portfolio

---
+++ Johnny.Portfolio.CoursePlayer.Core/Models/SSImage.cs
@@ -0,0 +1,16 @@
+﻿namespace Johnny.Portfolio.CoursePlayer.Core.Models
+{
+    public class SSImage
+    {
+        public SSImage(int row, int col, byte[] image)
+        {
+            Row = row;
+            Col = col;
+            Image = image;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
